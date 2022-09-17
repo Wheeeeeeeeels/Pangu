a7f7f53b882ab8c3a1e8f1facbd6e26578493073
Wheeeeeeeeels <ada_wheels@163.com>
2022-09-17 19:31:03 +0800

    add the unique_ptr

--- src/engine/allocator.h
+++ src/engine/allocator.h
@@ -1,6 +1,7 @@
 //
 // Created by wheels on 2022/9/16.
 // 空间配置器实现: 内存开辟与释放，对象构造与析构
+#include <cstdio>
 #include "pangu.h"
 
 
@@ -13,9 +14,8 @@ namespace Pangu{struct NewPlaceHolder{};}
 // void* 可以指向任意类型的地址,void* 只有强制类型转换之后才可以取值
 // 重载 new运算符
 inline void* operator new(size_t,Pangu::NewPlaceHolder,void* where){return where;}
+// 重载delete运算符
 inline void operator delete(void*,Pangu::NewPlaceHolder,void*){}
-
-
 
 namespace Pangu{
 
@@ -44,11 +44,45 @@ struct Local{
 	~Local(){
 		if(obj) obj->~T();
 	}
+    void operator=(const Local&) = delete;
+
+    template<typename... Args>
+    void create(Args&&... args){
+        ASSERT(!obj);
+        obj = new (NewPlaceHolder(),mem) T(static_cast<Args&&>(args)...);
+    }
+    void destroy(){
+        ASSERT(obj);
+        obj->~T();
+        obj = nullptr;
+    }
+    T& operator*() {ASSERT(obj); return *obj; }
+    T* operator->() {ASSERT(obj); return obj; }
+    T* get() {return obj;}
 private:
 	alignas(T) pangu::u8 mem[sizeof (T)];
 	T* obj = nullptr;
 };
+// re-implementation the UniquePtr
+template<typename T>
+struct UniquePtr{
+    UniquePtr() : m_ptr(nullptr),m_allocator(nullptr){}
+    UniquePtr(T* obj,IAllocator* allocator):m_ptr(obj),m_allocator(allocator){}
 
+    template<typename T2>
+    UniquePtr( UniquePtr<T2>&& rhs){
+        *this = static_cast<UniquePtr<T2>&&>(rhs);
+    }
+    ~UniquePtr(){
+        if(m_ptr){
+           // PANGU_DELETE(*m_allocator,m_prt);
+        }
+    }
+
+private:
+    T* m_ptr = nullptr;
+    IAllocator* m_allocator = nullptr;
+};
 
 }
 
